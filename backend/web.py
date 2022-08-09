c7369b412bd4b82270990dc10ec222ac0f15d5b5
schieljn <diemilbe@gmx.net>
2022-08-09 02:47:28 +0200

    fixed database enironment variable

--- backend/web.py
+++ backend/web.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-if os.environ.get("db_name"):
+if os.environ.get("DB_CONNECTION"):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_CONNECTION")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
