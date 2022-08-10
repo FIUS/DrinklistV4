f2afa9ced64e9306680c2e3394559f628fc532e5
schieljn <diemilbe@gmx.net>
2022-08-10 22:40:04 +0200

    checkout

---
+++ frontend/src/Components/Common/Internationalization/i18n.ts
@@ -0,0 +1,15 @@
+export const ERROR_MESSAGE = "Ein fehler ist aufgetreten";
+export const NAME = "Name";
+export const VALUE = "Betrag";
+export const RECHNUNGEN = "Rechnungen";
+export const RECHNUNGS_NAME = "Rechnungsname";
+export const HINZUFUEGEN = "Hinzufügen";
+export const ENTFERNEN = "Entfernen";
+export const KASSE_NACH_ABRECHNUNG = "Kasse nach Abrechnung";
+export const KASSE_GEZAEHLT = "Kasse nach Einzahlung gezählt";
+export const NEUER_KASSENSTAND = "Neuer Kassenstand";
+export const DIFFERENZ = "Differenz";
+export const ABRECHNUNG_HINZUGEFUEGT = "Abrechnung hinzugefügt";
+export const ABRECHNUNG_ABSCHLIESSEN = "Abrechnung abschließen";
+export const NEUE_ABRECHNUNG = "Neue Abrechnung";
+
