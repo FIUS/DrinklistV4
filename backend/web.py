8ff0825d6aaa6dbf8b94e0c94c195c8994e9e399
jschiel <justin.schiel@iteratec.com>
2023-01-25 17:03:35 +0100

    fixes in db file structure

--- backend/web.py
+++ backend/web.py
@@ -6,10 +6,10 @@ import os
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+os.makedirs("instance/db", exist_ok=True)
 if os.environ.get("DB_CONNECTION"):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_CONNECTION")
 else:
-    os.makedirs("instance/sqlite", exist_ok=True)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite/database.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/database.db'
 
 sql_database = SQLAlchemy(app)
