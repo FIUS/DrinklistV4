e4ff149f686b41b0620fc5a5d35fbecfc48f6439
jschiel <justin.schiel@iteratec.com>
2023-08-16 16:29:59 +0200

    added history on main overview

--- frontend/src/Components/User/Overview/Overview.tsx
+++ frontend/src/Components/User/Overview/Overview.tsx
@@ -1,14 +1,16 @@
-import { Grow, TextField, Typography } from '@mui/material'
+import { Box, Fab, Grow, Slide, TextField, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import UserButton from '../UserButton/UserButton'
 import style from './overview.module.scss'
 import { useDispatch, useSelector } from 'react-redux';
 import { CommonReducerType } from '../../../Reducer/CommonReducer';
-import { doGetRequest } from '../../Common/StaticFunctions';
+import { datetimeToString, doGetRequest } from '../../Common/StaticFunctions';
 import { setDrinkCategories, setDrinks, setMembers } from '../../../Actions/CommonAction';
 import { RootState } from '../../../Reducer/reducerCombiner';
 import { NAME, WER_BIST_DU } from '../../Common/Internationalization/i18n';
-import { Member } from '../../../types/ResponseTypes';
+import { Member, Transaction } from '../../../types/ResponseTypes';
+import HistoryIcon from '@mui/icons-material/History';
+import Spacer from '../../Common/Spacer';
 
 type Props = {}
 
@@ -16,6 +18,36 @@ const Overview = (props: Props) => {
     const dispatch = useDispatch()
     const common: CommonReducerType = useSelector((state: RootState) => state.common);
     const [searchfield, setsearchfield] = useState("")
+    const anchor = React.useRef(null);
+    const [history, sethistory] = useState<Array<Transaction> | null>(null)
+
+    const memberHistoryName = () => {
+        const member = common.members?.find((member) => historyItemToDisplay?.memberID === member.id)
+        if (member?.alias !== "") {
+            return member?.alias
+        }
+        else return member.name
+    }
+
+    const [historyState, sethistoryState] = useState(0)
+    const historyItemToDisplay = history?.at(Math.floor(historyState / 3))
+
+    useEffect(() => {
+
+        setTimeout(() => {
+            sethistoryState((historyState + 1) % 9)
+        }, (historyState % 3 === 1) ? 10000 : 200);
+
+    }, [historyState])
+
+    useEffect(() => {
+        doGetRequest("transactions/limit/3").then((value) => {
+            if (value.code === 200) {
+                sethistory(value.content)
+            }
+        })
+    }, [])
+
 
     useEffect(() => {
         if (common.drinks === null || common.members === null || common.drinkCategories === null) {
@@ -43,40 +75,73 @@ const Overview = (props: Props) => {
             member.alias.toLowerCase().includes(searchfield.toLowerCase())
     }
 
+    const historyBox = () => {
+        if (history !== null) {
+            return <Box sx={{ position: 'fixed', bottom: '10px', left: '10px' }}>
+                <Fab variant="extended"
+                    size="medium"
+                    color="default"
+                    ref={anchor}
+                    style={{ 'overflow': 'hidden' }}
+                >
+                    <HistoryIcon />
+                    <Spacer horizontal={10} />
+
+                    <Slide in={(historyState % 3) === 1}
+                        container={anchor.current}
+                        direction={(historyState % 3) < 2 ? "down" : "up"}
+                        timeout={200}>
+                        <Typography>
+                            {memberHistoryName() + " - " + historyItemToDisplay?.description + " - " + datetimeToString(historyItemToDisplay?.date)}
+                        </Typography>
+                    </Slide>
+                </Fab>
+
+
+            </Box>
+        } else {
+            return <></>
+        }
+    }
+
     return (
-        <div className={style.outterContainer}>
-            <div className={style.headline}>
-                <Typography variant='h4'>{WER_BIST_DU}</Typography>
-            </div>
+        <>
+            <div className={style.outterContainer}>
+                <div className={style.headline}>
+                    <Typography variant='h4'>{WER_BIST_DU}</Typography>
+                </div>
 
-            <TextField
-                className={style.input}
-                placeholder={NAME}
-                type="search"
-                value={searchfield}
-                onChange={
-                    (value) => { setsearchfield(value.target.value) }
-                }
-            />
-
-            <div className={style.buttonArea}>
-                {common.members?.sort((value1, value2) => {
-                    const name1 = value1.alias !== "" ? value1.alias : value1.name
-                    const name2 = value2.alias !== "" ? value2.alias : value2.name
-                    return name1.localeCompare(name2)
-                }
-                )?.map(value => {
-                    if (!value.hidden) {
-                        return <Grow in={userVisible(value)} key={value.id} unmountOnExit>
-                            <div style={{ width: "100%" }}>
-                                <UserButton key={value.id} name={value.alias === "" ? value.name : value.alias} id={value.id} />
-                            </div>
-                        </Grow>
+                <TextField
+                    className={style.input}
+                    placeholder={NAME}
+                    type="search"
+                    value={searchfield}
+                    onChange={
+                        (value) => { setsearchfield(value.target.value) }
                     }
-                    return <></>
-                })}
-            </div>
-        </div >
+                />
+
+                <div className={style.buttonArea}>
+                    {common.members?.sort((value1, value2) => {
+                        const name1 = value1.alias !== "" ? value1.alias : value1.name
+                        const name2 = value2.alias !== "" ? value2.alias : value2.name
+                        return name1.localeCompare(name2)
+                    }
+                    )?.map(value => {
+                        if (!value.hidden) {
+                            return <Grow in={userVisible(value)} key={value.id} unmountOnExit>
+                                <div style={{ width: "100%" }}>
+                                    <UserButton key={value.id} name={value.alias === "" ? value.name : value.alias} id={value.id} />
+                                </div>
+                            </Grow>
+                        }
+                        return <></>
+                    })}
+                </div>
+
+            </div >
+            {historyBox()}
+        </>
     )
 }
 
