7d9324e2562e770e1ff13c6686bf129cfd063899
themrphantom <themrphantom@github.com>
2024-04-26 17:42:55 +0200

    fixed login issue

--- frontend/src/Components/Common/Login/Login.tsx
+++ frontend/src/Components/Common/Login/Login.tsx
@@ -19,7 +19,23 @@ const Login = (props: Props) => {
     const [password, setpassword] = useState("")
 
     const login = () => {
-        setdisableLoginButton(true)
+        setdisableLoginButton(true);
+        (function () {
+            var cookies = document.cookie.split("; ");
+            for (var c = 0; c < cookies.length; c++) {
+                var d = window.location.hostname.split(".");
+                while (d.length > 0) {
+                    var cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path=';
+                    var p = window.location.pathname.split('/');
+                    document.cookie = cookieBase + '/';
+                    while (p.length > 0) {
+                        document.cookie = cookieBase + p.join('/');
+                        p.pop();
+                    };
+                    d.shift();
+                }
+            }
+        })();
         doPostRequest("login", { name: username, password: password }).then((value) => {
             if (value.code === 200) {
                 const searchParam = searchParams.get("originalPath")
