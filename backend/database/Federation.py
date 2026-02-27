4b04ab8854cccb44cb8e33af104ef0bac576e5d4
Justin Schiel <justin.schiel@ipvs.uni-stuttgart.de>
2026-02-27 19:16:34 +0100

    Added Federation Model

---
+++ backend/database/Federation.py
@@ -0,0 +1,24 @@
+import sqlalchemy as sql
+from web import sql_database as db
+from sqlalchemy.orm import relationship
+import util
+
+
+class Federation(db.Model):
+    id = sql.Column(sql.Integer, primary_key=True)
+    name = sql.Column(sql.String(100), nullable=False)
+    url = sql.Column(sql.String(200), nullable=False)
+    federation_user_id = sql.Column(sql.Integer, sql.ForeignKey(
+        'member.id', ondelete='CASCADE'), nullable=False)
+    federation_user = relationship(
+        'database.Member.Member', lazy="joined")
+    accepted = sql.Column(sql.Boolean, nullable=False, default=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "url": self.url,
+            "federation_user_id": self.federation_user_id,
+            "accepted": self.accepted
+        }
