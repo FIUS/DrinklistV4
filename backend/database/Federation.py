54ffb91d11f803481bdf203bda39c042ce39c2ef
Justin Schiel <justin.schiel@ipvs.uni-stuttgart.de>
2026-02-27 21:02:29 +0100

    federation model change

--- backend/database/Federation.py
+++ backend/database/Federation.py
@@ -8,7 +8,8 @@ class Federation(db.Model):
     id = sql.Column(sql.Integer, primary_key=True)
     name = sql.Column(sql.String(100), nullable=False)
     url = sql.Column(sql.String(200), nullable=False)
-    remote_password = sql.Column(sql.String(100), nullable=False)
+    remote_password = sql.Column(sql.String(100), nullable=True)
+    remote_id = sql.Column(sql.Integer, nullable=True)
     federation_user_id = sql.Column(sql.Integer, sql.ForeignKey(
         'member.id', ondelete='CASCADE'), nullable=False)
     federation_user = relationship(
@@ -20,6 +21,7 @@ class Federation(db.Model):
             "id": self.id,
             "name": self.name,
             "url": self.url,
+            "remote_id": self.remote_id,
             "federation_user_id": self.federation_user_id,
             "accepted": self.accepted
         }
